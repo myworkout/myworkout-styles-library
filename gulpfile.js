9a969bf82c7af06b165f1908d7c6dd3426793df6
Vladimir Janjusevic <vladimir@myworkout.no>
2017-11-23 14:00:02 +0100

    MYGO-1487 Create basic layout for docs.html Add tasks to build docs assets

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var gulp = require('gulp'),
 
 // Styles
 gulp.task('styles', function() {
-    return gulp.src(['./dev/main.scss'])
+    return gulp.src(['./dev/sass/main.scss'])
         .pipe($.sass())
         .pipe($.autoprefixer('last 6 version'))
         .pipe($.concatCss('bundle.css'))
@@ -21,18 +21,54 @@ gulp.task('clean', function(){
     return del('./dist/');
 });
 
-// Move files
-gulp.task('files', function() {
+// Build Docs
+//_____________________________________________________
+
+// Move docs.html
+gulp.task('move-docs', function() {
     return gulp.src(['./dev/docs.html'])
         .pipe(gulp.dest('./dist/'));
 });
 
+// Docs scripts
+gulp.task('docs-scripts', function() {
+    return gulp.src([
+        './node_modules/jquery/dist/jquery.js',
+        './dev/docs-assets/scripts.js'
+    ])
+        .pipe($.concat('docs.js'))
+        .pipe($.rename({ suffix: '.min' }))
+        .pipe($.uglify())
+        .pipe(gulp.dest('./dist/docs-assets/'));
+});
+
+// Docs styles
+gulp.task('docs-styles', function() {
+    return gulp.src(['./node_modules/normalize.css/normalize.css', './dev/docs-assets/styles.scss'])
+        .pipe($.sass())
+        .pipe($.autoprefixer('last 6 version'))
+        .pipe($.concatCss('docs.css'))
+        .pipe($.rename({ suffix: '.min' }))
+        .pipe($.cssnano())
+        .pipe(gulp.dest('./dist/docs-assets/'));
+});
+
+// Docs all
+gulp.task ('docs', function (cb) {
+    runSequence('move-docs', 'docs-scripts', 'docs-styles', cb);
+});
+
+//_____________________________________________________
+
 // Default
 gulp.task ('default', function (cb) {
-    runSequence('clean', 'styles', 'files', cb);
+    runSequence('clean', 'styles', 'docs', cb);
 });
 
 // Watch
 gulp.task('watch', function() {
-    gulp.watch('./**/*.scss', ['styles']);
+    gulp.watch('./dev/sass/**/*.scss', ['styles']);
+    gulp.watch('./dev/docs-assets/scripts.js', ['docs-scripts']);
+    gulp.watch('./dev/docs-assets/styles.scss', ['docs-styles']);
+    gulp.watch('./dev/docs.html', ['move-docs']);
 });
